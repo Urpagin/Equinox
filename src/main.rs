1dff4cedcbf0cdd646920e3cd68c5fa81987cd9f
urpagin <urpagin@domain.com>
2024-09-20 13:35:10 +0200

    e

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
+#[poise::command(context_menu_command)]
 async fn age(
     ctx: Context<'_>,
     #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
+    ctx.say("Hello!").await?;
     Ok(())
 }
 
